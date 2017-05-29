1721113f1adf21de22dbb6e5ccab572571cb6c69
jpatricia <janice_patricia@live.com>
2017-05-29 00:28:59 -0400

    solution with white background

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # akindi-pre-interview
+
+1. Install http-server `npm install http-server -g`
+2. Run `http-server`
+3. Open `http://localhost:8080`
---
+++ app.js
@@ -0,0 +1,34 @@
+var app = angular.module('app', []);
+app
+	.controller('mainController', function($scope) {
+		$.getJSON('data.json', function(res) {
+			var counter = 0;
+			$scope.data = res.tableData.map(function(row) {
+				row.status = row.activeStatus ? 'Active' : 'Inactive';
+				var d = new Date(row.lastSeen);
+				row.lastActive = d.toDateString();
+				return row;
+			});
+			$scope.$apply();
+		});
+
+		$scope.changeActiveStatus = function(row) {
+			var cloneData = _.cloneDeep($scope.data);
+			cloneData.activeStatus = !cloneData.activeStatus;
+			cloneData.status = cloneData.activeStatus ? 'Active' : 'Inactive';
+		};
+
+		$scope.addClass = function(status) {
+			if(status) return 'active';
+			return 'inactive';
+		};
+
+		$scope.rowClass = function(status) {
+			if(status) return '';
+			return 'nonactive';
+		};
+
+		$scope.changeSorting = function(colName) {
+			$scope.data = _.sortBy($scope.data,[colName]);
+		};
+	});
